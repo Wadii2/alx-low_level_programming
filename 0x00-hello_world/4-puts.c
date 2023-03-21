afcbdec0802dc620d7d9161c67bca3be46f41230
wadii2 <zainewadii@gmail.com>
2023-03-20 18:11:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
- * retune 0.
- */
-
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
