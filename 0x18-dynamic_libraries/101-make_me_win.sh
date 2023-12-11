8ba9ba3c7bba031ae95fd9dea110e9320e9f5a60
wadii2 <zainewdii@gmail.com>
2023-12-11 10:43:50 -0800

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/wadii2/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -N -P /tmp https://raw.githubusercontent.com/wadii2/alx-low_level_programming/master/0x18-dynamic_libraries/randomx.so
+export LD_PRELOAD="/tmp/randomx.so"
